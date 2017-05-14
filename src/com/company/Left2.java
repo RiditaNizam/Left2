4a0c991d2525bdf4c2c28983196cd3c780bfb00e
RiditaNizam <riditan@gmail.com>
2017-05-13 23:26:43 -0400

    Initial commit

---
+++ src/com/company/Left2.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by ridita on 5/13/17.
+ */
+public class Left2 {
+    public String left2(String str) {
+        return str.substring(2) + str.substring(0, 2);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Left2 left2Object = new Left2();
+    }
+}
